8e339e5666cff70230108ff9cbf2e415d2f4e215
Joni Lillsund <joni.lillsund@gmail.com>
2023-05-04 19:29:11 +0300

    Up-Down Movement Added
    -Up/Down Arrows work
    - Input System setup
    - Shoot event Ready to use

--- Assets/Scipts/Player_controller.cs
+++ Assets/Scipts/Player_controller.cs
@@ -2,16 +2,39 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 
 public class Player_controller : MonoBehaviour
 {
-   /** private TouchControl touchControl;
+    public float speed = 1f;
+    private Rigidbody2D rb;
+    Vector2 moveInput;
 
-    private void Awake()
+    private void Start()
     {
-        touchControl = new TouchControl();
+        rb = GetComponent<Rigidbody2D>();
     }
-   */
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, moveInput.y * speed);
+    }
+
+    public void OnUp(InputAction.CallbackContext context)
+    {
+        Debug.Log("up");
+        float moveY = context.ReadValue<float>();
+        moveInput.y = moveY;
+    }
+    public void OnDown(InputAction.CallbackContext context)
+    {
+        Debug.Log("down");
+        float moveY = context.ReadValue<float>();
+        moveInput.y = -moveY;
+    }
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        // Ampuu
+    }
 }
-   
