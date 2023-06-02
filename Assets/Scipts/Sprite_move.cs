4172de207bf54346d5b3d12c4a55bf0a4c00dae3
Joni Lillsund <joni.lillsund@gmail.com>
2023-06-02 17:38:40 +0300

    Ground move
    - Added Ground(Quad 3D object)
    - Added MeshRendered ground to object
    - Etualamaa wrap mode repeat

---
+++ Assets/Scipts/Sprite_move.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Sprite_move : MonoBehaviour
+{
+    public float speed;
+    float singleTextureWidth;
+
+    private void Start()
+    {
+        setupTexture();
+    }
+
+    void setupTexture()
+    {
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        singleTextureWidth = sprite.texture.width / sprite.pixelsPerUnit;
+    }
+
+    void Scroll()
+    {
+        float delta = speed * Time.deltaTime;
+        transform.position += new Vector3(-delta, 0, 0);
+    }
+
+    void CheckReset()
+    {
+        if((Mathf.Abs(transform.position.x) - singleTextureWidth) > 0)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void Update()
+    {
+        Scroll();
+        CheckReset();
+    }
+}
